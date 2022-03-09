2f3ca1697c0f5bfcac4725539003a13d4dfbd508
Carlos Mugica <cmugica4@cloud.com>
2022-03-09 13:28:57 -0700

    Initial Commit

---
+++ intergalactic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  intergalactic
+//
+//  Created by Carlos Mugica on 3/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
